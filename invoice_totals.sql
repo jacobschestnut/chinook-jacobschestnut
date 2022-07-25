b9771929e90ce4172845dbabc4199afd2846743c
Jake Chestnut <jacobschestnut@gmail.com>
2022-07-25 18:40:56 -0500

    invoice_totals.sql

---
+++ invoice_totals.sql
@@ -0,0 +1,9 @@
+Invoice Total
+Customer Name
+Customer Country
+Sale Agent full name
+
+SELECT i.Total, c.FirstName ||" "|| c.LastName as CustomerFullName, c.Country, e.FirstName ||" "|| e.LastName as EmployeeFullName
+    FROM Invoice i
+    JOIN Customer c ON i.CustomerId = c.CustomerId
+    JOIN Employee e ON e.EmployeeId = c.SupportRepId;
